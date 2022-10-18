c95034a92b08193a2b9392d4ff431e361ec90c86
half144 <99549268+half144@users.noreply.github.com>
2022-10-18 18:56:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const products = require("./products");
 const port = process.env.PORT || 3000
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+
 app.get("/iphones", function (req, res) {
   res.json(products.iphones);
 });
